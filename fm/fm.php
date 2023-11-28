75412c351b23d40fe918d54504c86341d5933410
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-11-28 11:34:24 +0700

    NuLz

--- fm/fm.php
+++ fm/fm.php
@@ -1,5 +1,5 @@
 <?php
-//{"lang":"","fm_root":"","timezone":"","date_format":"Y\/m\/d H:i","auth_pass":"8286514ebdf5dba25de769163e63b0b3","error_reporting":1}
+{"lang":"","fm_root":"","timezone":"Asia/Jakarta","date_format":"Y\/m\/d H:i","auth_pass":"ff6d9718a96dd1c0ec471cc444adaf54","error_reporting":0}
 /*-------------------------------------------------
 | PHP FILE MANAGER
 +--------------------------------------------------
