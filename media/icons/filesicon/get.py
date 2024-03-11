666b2fcf6a5adadfb1735229ca211f08255e3d55
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-03-12 03:39:43 +0700

    NuLz

--- media/icons/filesicon/get.py
+++
@@ -1,6 +0,0 @@
-list = ["json","ppt","pptx","xls","xlsx","msi","config","cgi","pm","c","cpp","cs","java","aspx","asp","db","ttf","eot","woff","woff2","woff","conf","log","apk","cab","bz2","tgz","dmg","izo","jar","7z","iso","rar","bat","sh","alfa","gz","tar","php","php4","php5","phtml","html","xhtml","shtml","htm","zip","png","jpg","jpeg","gif","bmp","ico","txt","js","rb","py","xml","css","sql","htaccess","pl","ini","dll","exe","mp3","mp4","m4a","mov","flv","swf","mkv","avi","wmv","mpg","mpeg","dat","pdf","3gp","doc","docx","docm"]
-
-for icon in list:
-    iconlist = f'http://solevisible.com/icons/{icon}.png'
-    open('icon.txt', 'a').write(str(iconlist+'\n'))
-    print(iconlist)
